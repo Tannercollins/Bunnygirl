a9aeb0d9957b15122dfdc36624e006f762ada394
Tannercollins <TannerCollinsNA@gmail.com>
2017-02-09 15:38:01 -0700

    Newest

--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@ function computePrice() {
 var caseprice = document.getElementById('casePrice').value;
 var each = document.getElementById('Each').value;
 var perEach = (caseprice / each);
-var Price = (perEach * 0.6316).toFixed(2);
+var prePrice = (perEach / 0.6316);
+var longPrice = Math.round(prePrice*10)/10-.01;
+var Price = (longPrice).toFixed(2);
+
+
 Price = Price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 document.getElementById('Price1').innerHTML = "Price to charge per Each or Lbs = $"+Price;
-
 };
 
-function dodobird() {
-  document.getElementById('Price1').innerHTML = "Price to charge per Each or Lbs = $";
-};
+
+function reset() {
+     document.getElementById("casePrice").value = "";
+     document.getElementById("Each").value = "";
+     document.getElementById("Price1").value = "";
+}
